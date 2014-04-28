954dc468083d1600c42be8d3565847503a65b836
Tasha25 <greennatasha@aol.com>
2014-04-28 00:32:38 -0400

    Commented out rakefile.rb

--- rakefile.rb
+++ rakefile.rb
@@ -3,21 +3,21 @@ require './main'
 require 'bundler/setup'
 require 'active_record'
 
-include ActiveRecord::Tasks
+# include ActiveRecord::Tasks
 
-db_dir = File.expand_path('../db', __FILE__)
-config_dir = File.expand_path('../config', __FILE__)
+# db_dir = File.expand_path('../db', __FILE__)
+# config_dir = File.expand_path('../config', __FILE__)
 
-DatabaseTasks.env = ENV['ENV'] || :development
-DatabaseTasks.db_dir = db_dir
-DatabaseTasks.database_configuration = YAML.load(File.read(File.join(config_dir, 'database.yml')))
-DatabaseTasks.migrations_paths = File.join(db_dir, 'migrate')
+# DatabaseTasks.env = ENV['ENV'] || :development
+# DatabaseTasks.db_dir = db_dir
+# DatabaseTasks.database_configuration = YAML.load(File.read(File.join(config_dir, 'database.yml')))
+# DatabaseTasks.migrations_paths = File.join(db_dir, 'migrate')
 
-task :environment do
-  ActiveRecord::Base.configurations = DatabaseTasks.database_configuration
-  ActiveRecord::Base.establish_connection DatabaseTasks.env
-end
+# task :environment do
+#   ActiveRecord::Base.configurations = DatabaseTasks.database_configuration
+#   ActiveRecord::Base.establish_connection DatabaseTasks.env
+# end
 
-load 'active_record/railties/databases.rake'
+# load 'active_record/railties/databases.rake'
 
 #this needs to be in the rakefile.rb instead of the other things because you will get an "uninitialized constant ActiveRecord::Tasks::DatabaseTasks::Rails
